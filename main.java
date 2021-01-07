1c73084321c228b2125fe10f5abdf22bc54d15bc
helenkhim <elenbogacheva97@yandex.ru>
2021-01-07 18:09:55 +0300

    добавлние кода

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        int cash = 20;
+        int amount = 10000;
+        int bonus = amount / cash;
+        System.out.println(bonus);
+    }
+}
